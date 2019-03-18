f09095c6938ceed67a0c7fe684e714e2e557db71
LeeAnne Galioto <10750369+galiotoweb@users.noreply.github.com>
2019-03-18 15:05:51 -0400

    Roster page data from jsonplaceholder

--- src/pages/Roster.jsx
+++ src/pages/Roster.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
-const Roster = () => (
-  <React.Fragment>
-    <h1>Roster Page</h1>
-  </React.Fragment>
-);
+class Roster extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      players: [],
+    };
+  }
+
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/users').then((res) => {
+      const players = res.data;
+      this.setState({ players });
+    });
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Player Roster</h1>
+        <ul>
+          {this.state.players.map(player => (
+            <li>{player.name}</li>
+          ))}
+        </ul>
+        <Link to="/player/new">
+          <button>Add A New Player</button>
+        </Link>
+      </React.Fragment>
+    );
+  }
+}
 
 export default Roster;
